04260527758e21c2886cd1d49269681db304ffb9
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2021-06-24 08:47:33 +0600

    Update ffmpeg.py

--- core/ffmpeg.py
+++ core/ffmpeg.py
@@ -48,10 +48,7 @@ async def vidmark(the_media, message, working_dir, watermark_path, output_vid, t
         "20",
         "-aspect",
         "16:9",
-        "-map",
-        "0:v",
-        "-map",
-        "0:m:language:eng",
+        "-vf scale=854:480",
         output_vid
     ]
     COMPRESSION_START_TIME = time.time()
@@ -132,8 +129,6 @@ async def take_screen_shot(video_file, output_directory, ttl):
         str(ttl),
         "-i",
         video_file,
-        "-vf",
-        "scale=854:480:flags=lanczos",
         out_put_file_name
     ]
     # width = "90"
